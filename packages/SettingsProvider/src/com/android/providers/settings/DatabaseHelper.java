99fcca660d82a2e6b614866c581129d68a45ecdb
pengchao2 <pengchao2@autoai.com>
2024-10-28 19:22:55 +0800

    配置默认输入法为sougou输入法

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -2398,7 +2398,8 @@ class DatabaseHelper extends SQLiteOpenHelper {
 
             loadIntegerSetting(stmt, Settings.Secure.SLEEP_TIMEOUT,
                     R.integer.def_sleep_timeout);
-
+            loadStringSetting(stmt, Settings.Secure.DEFAULT_INPUT_METHOD,R.string.config_default_input_method);
+            loadStringSetting(stmt, Settings.Secure.ENABLED_INPUT_METHODS,R.string.def_enabled_input_methods);
             /*
              * IMPORTANT: Do not add any more upgrade steps here as the global,
              * secure, and system settings are no longer stored in a database
